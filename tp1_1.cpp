f569ae7888a7ecbcd99a7fb11323af0c635e2fbb
Alejandro <ale_arc2015@hotmail.com>
2019-03-14 23:12:23 -0300

    Punto 1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,2 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int main (){
+	//Inicializo Variables
+	int valor = 10;
+	int *puntero;
+	//Le asigno la direccion de memoria de "valor" al puntero
+	puntero = &valor;
+	//Muestro el contenido del puntero
+	printf("%d\n",puntero);
+	//Muestro la direccion de memoria almacenada por el puntero
+	printf("%p\n",puntero);
+	//Muestro la direccion de memoria de la variable
+	printf("%p\n",valor);
+	//Muestro la direccion de memoria del puntero
+	printf("%p\n"&puntero);
+	//Muestro el tamaño de memoria utilizado por "valor"
+	sizeof(valor);
+	//Respuestas
+	//El resultado del punto 2 y 3 es el mismo debido a que "puntero" almacena la direccion de memoria de la variable "valor" y no su contenido.
+	//El resultado obtenido del punto 4 es distinto al resto dado que no se muestra lo que contiene "puntero" sino la direccion de memoria del mismo.
+
+
+
+	return;
+}
