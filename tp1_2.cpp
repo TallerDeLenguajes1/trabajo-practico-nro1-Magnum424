ad65825724e8da30e8a196f7671856d2bdd4f817
Alejandro <ale_arc2015@hotmail.com>
2019-03-18 20:01:49 -0300

    Update

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 int main (void){
-	
+	int numerito = 2;
+	Cuadrado_int(numerito);
 	return;
+}
+int Cuadrado_int(int x){
+	int cuadrado;
+	cuadrado = x * x;
+	return cuadrado;
+}
+void Cuadrado(){
+	int x;
+	printf("Ingrese el numero a elevar:\n");
+	scanf("%d",x);
+	x = x * x;
+	printf("Su numero elevado es: %d",x);
+}
+void mostrar_datos(int x){
+	printf("%d",x);
+	printf("%p",&x);
 }
