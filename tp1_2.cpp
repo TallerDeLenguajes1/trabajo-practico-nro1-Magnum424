73a8971dbe7b1949c5e7189eb227cfe9659b7d38
Alejandro <ale_arc2015@hotmail.com>
2019-03-19 21:41:57 -0300

    tp1

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -4,6 +4,9 @@
 int Cuadrado_int(int x);
 void Cuadrado();
 void mostrar_datos(int x);
+void invertir(int a, int b);
+void menor_mayor(int a,int b);
+void menor_mayor_entrada();
 
 int main (void){
 	int numerito = 2;
@@ -13,19 +16,62 @@ int main (void){
 
 	return 0;
 }
+
 int Cuadrado_int(int x){
 	int cuadrado;
 	cuadrado = x * x;
 	return cuadrado;
 }
+
 void Cuadrado(){
 	int x;
 	printf("Ingrese el numero a elevar:\n");
-	scanf("%d",x);
+	scanf("%d",&x);
 	x = x * x;
 	printf("Su numero elevado es: %d",x);
 }
+
 void mostrar_datos(int x){
 	printf("%d",x);
 	printf("%d",&x);
+}
+
+void invertir(int a, int b){
+	int c;
+	c = b;
+	b = a;
+	a = b;
+	printf("a=%d//b=%d",a,b);
+}
+
+void menor_mayor(int a,int b){
+	if (a<b)
+	{
+		printf("%d\n",a);
+		printf("%d\n",b);
+	}
+	else{
+		printf("%d\n",b);
+		printf("%d\n",a);
+	}
+}
+
+void menor_mayor_entrada(){
+	int a,b;
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("Ingrese el primer numero:");
+		scanf("%d",&a);
+		printf("Ingrese el segundo numero:");
+		scanf("%d",&b);
+		if (a<b)
+		{
+			printf("%d\n",a);
+			printf("%d\n",b);
+		}
+		else{
+			printf("%d\n",b);
+			printf("%d\n",a);
+		}
+	}	
 }
